caac4f16a2450467556602e010f24929b69d467c
Michael Kidd <michael.kidd@student.manchester.ac.uk>
2026-02-10 17:30:20 +0000

    ReadMe.md added

---
+++ README.md
@@ -0,0 +1,15 @@
+
+-- how to configure
+
+you need to install pyenv or be running python 3.11.x or lower, then install mbed-cli in that pyenv.
+Make sure that when you do "mbed ls" your version is 6.17. Then do
+
+mbed target NUCLEO_F401RE 
+
+mbed toolchain GCC_ARM
+
+you will need to install none-eabi-gcc
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,5 @@
 #include "rgb_led.hpp"
 
 int main() {
-  RGB_Led led;
-
-  for (;;) {
-    led.off();
-    ThisThread::sleep_for(1000ms);
-    led.on(RGB_Led::RED);
-    ThisThread::sleep_for(1000ms);
-    led.off();
-    led.on(RGB_Led::GREEN);
-    ThisThread::sleep_for(1000ms);
-    led.off();
-    led.on(RGB_Led::BLUE);
-    ThisThread::sleep_for(1000ms);
-  }
+  return 0;
 }
