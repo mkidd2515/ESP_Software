933c743331cc0c45314fe9905c56fff4f848ad9c
Michael Kidd <michael.kidd@student.manchester.ac.uk>
2026-02-11 16:56:05 +0000

    qei added

--- src/motor.cpp
+++ src/motor.cpp
@@ -8,8 +8,21 @@ Motor::Motor(PinName pwmPin, int period_us, float duty, PinName chA, PinName chB
 	   dirOut(NC),
 	   pwm(pwmPin),
 	   quad(chA, chB, NC, 256)
-
 {
 	pwm.period_us(period_us);
-	
+		
 }
+
+void Motor::set_speed(float speed) {
+	pwm.write(speed);
+	return;	
+}
+
+int get_speed() {
+	int n0, n1;
+	n0 = n1 = 0;
+	timer.start;
+	n0 = quad.getPulses();
+	wait(5 us)
+}
+
