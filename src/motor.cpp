eb2b40229e4f1ab7bf41c67b95de19155777d674
Michael Kidd <michael.kidd@student.manchester.ac.uk>
2026-02-11 16:38:14 +0000

    Fixed qei lib and fixed motor constructor

--- src/motor.cpp
+++ src/motor.cpp
@@ -1,11 +1,14 @@
 #include "mbed.h"
 #include "QEI.h"
+#include "motor.hpp"
 
 // FYI constructor not complete 
 Motor::Motor(PinName pwmPin, int period_us, float duty, PinName chA, PinName chB) 
-	: pwm(pwmPin)
-	, quad(chA, chB, NULL, 256)
-	, dir(0) 
+	:  dir(false),
+	   dirOut(NC),
+	   pwm(pwmPin),
+	   quad(chA, chB, NC, 256)
+
 {
 	pwm.period_us(period_us);
 	
