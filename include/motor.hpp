ec51e18947c1a0443c2027838d787a05fe50850e
Michael Kidd <michael.kidd@student.manchester.ac.uk>
2026-02-11 23:40:18 +0000

    speed sesing draft done

--- include/motor.hpp
+++ include/motor.hpp
@@ -1,5 +1,16 @@
+#ifndef MOTOR_HPP
+#define MOTOR_HPP
+
+
 #include "mbed.h"
 #include "QEI.h"
+
+
+
+#include <cstdint>
+
+static constexpr int TICKER_PERIOD_US = 100;
+
 
 class Motor {
 protected:
@@ -8,16 +19,30 @@ protected:
 	DigitalOut dirOut; // GPIO pin for setting direction
 	PwmOut pwm; // speed control of motor
 	QEI quad;  // reading the quad enc
-	Timer timer;
+	Ticker pulse_sampler;
+	int pulse_count;
+	float current_speed;
+        
+	void calc_speed(void) noexcept;
 	
 public:
-	Motor(PinName dirPin, PinName pwmPin, int period_us, float duty, PinName chA, PinName chB);
+	Motor(PinName dirPin, PinName pwmPin, int period_us, float duty, PinName chA, PinName chB) noexcept;
+	// delete misc constructors, OBJECT SHOULD NOT BE COPIED
+	Motor(const Motor&) = delete;
+	Motor operator=(const Motor&) = delete;
+	Motor(const Motor&&) = delete;
+	Motor& operator=(const Motor&&) = delete;
+        
 	
-	void set_dir(int newD);
-	int get_dir(void);
 
-	int get_speed(void);
-	void set_speed(float speed); // input speed as percentage but might switch to RPM
+	void set_dir(int newD) noexcept;
+	int get_dir(void) const noexcept;
+
+	float get_speed(void) const noexcept;
+	void set_speed(float speed) noexcept; // input speed as percentage but might switch to RPM
 	
-	int 
 };
+
+
+
+#endif
--- src/motor.cpp
+++ src/motor.cpp
@@ -2,29 +2,46 @@
 #include "QEI.h"
 #include "motor.hpp"
 
+#include <chrono>
+
+
 // FYI constructor not complete 
-Motor::Motor(PinName dirPin, PinName pwmPin, int period_us, float duty, PinName chA, PinName chB) 
-		dutyCycle(duty),
+Motor::Motor(PinName dirPin, PinName pwmPin, int period_us, float duty, PinName chA, PinName chB) noexcept : 
+  	    dutyCycle(duty),
 	    period(period_us),
 	    dirOut(dirPin),
 	    pwm(pwmPin),
-	    quad(chA, chB, NC, 256)
+	    quad(chA, chB, NC, 256),
+	    pulse_count(quad.getPulses())
 {
 	pwm.period_us(period_us);
-	t_prev = t_now = Kernel::get_ms_count();
-	pCntPrev = pCntNow = quad.getPulses();
+	// t_prev = t_now = Kernel::get_ms_count();
+	// pCntPrev = pCntNow = quad.getPulses();
+  pulse_sampler.attach(callback(this, &Motor::calc_speed), std::chrono::microseconds(TICKER_PERIOD_US));
 }
 
-void Motor::set_dir(int newD) {
-	dirOut = newD & 0x1;
+
+void Motor::calc_speed(void) noexcept {
+  int current_pulses = quad.getPulses();
+  int dPulses = current_pulses - pulse_count;
+  const float pulses_per_second = 
+  static_cast<float>(dPulses) / (static_cast<float>(TICKER_PERIOD_US) * 1e-6);
+  current_speed = pulses_per_second / 256.0f;
+  pulse_count = current_pulses;
+}
+
+
+
+void Motor::set_dir(int newD) noexcept{
+	dirOut = static_cast<int>((newD != 0));
 	return;
 }
 
-int Motor::get_dir(void) { 
-	return dirOut; 
+int Motor::get_dir(void) const noexcept { 
+	return const_cast<DigitalOut&>(dirOut).read(); 
 }
 
-void Motor::set_speed(float speed) {
+void Motor::set_speed(float speed) noexcept{
 	pwm.write(speed);
 	return;	
 }
@@ -38,7 +55,20 @@ void Motor::set_speed(float speed) {
  * or a little less than HALF OUR MAX SPEED (1.17 m/s)
  * not to mention that the longer we take to sample, the more our latency is
 */ 
-float Motor::get_speed(void) {
+
+float Motor::get_speed(void) const noexcept{
+  return current_speed;
+}
+
+
+
+
+
+
+
+
+
+
 	/*int n0, n1;
 	n0 = n1 = 0;
 	timer.start();
@@ -48,6 +78,4 @@ float Motor::get_speed(void) {
 	timer.stop();
 	float speed = 0;
 	*/
-	
-}
 
